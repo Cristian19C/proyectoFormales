33b53ced0136fc5e93971a13d134704895ed79c3
Cristian <cristian.cordoba01@uptc.edu.co>
2023-08-25 08:09:04 -0500

    Analisisdel lenguaje por expresiones regulares

--- Note.py
+++ Note.py
@@ -1,12 +1,10 @@
-import pygments
+# import pygments
 import os
+import re
 
 from tkinter import *
-
 from tkinter import messagebox
-
 from tkinter.filedialog import askopenfile, asksaveasfile
-
 from pygments.lexers import get_lexer_for_filename
 from pygments.util import ClassNotFound
 
@@ -40,19 +38,46 @@ def nuevo():
 
     editor.delete(1.0,END)
 
+def detect_language(content):
+    patterns = {
+        'julia': r'\b(julia|end|function|if|else)\b',
+        'perl': r'\b(perl|print|sub|if|else)\b',
+        'ruby': r'\b(ruby|puts|def|if|else)\b'
+    }
+
+    detected_language = None
+
+    for language, pattern in patterns.items():
+        if re.search(pattern, content, re.IGNORECASE):
+            detected_language = language
+            break
+
+    return detected_language
+
 def abrir():
-
-    documento = askopenfile()
-    editor.delete(1.0, END)
-    contenido = documento.read()
-    editor.insert(1.0, contenido)
-
     try:
-        lexer = get_lexer_for_filename(documento.name)
-        detected_language = lexer.name
-        messagebox.showinfo("Detected Language", f"Este código parece estar escrito en {detected_language}.")
-    except ClassNotFound:
-        messagebox.showinfo("Detected Language", "No se pudo detectar el lenguaje con certeza.")
+        documento = askopenfile()
+        if documento:
+            print("Archivo abierto con éxito")
+            contenido = documento.read()
+            print("Contenido leído")
+            editor.delete(1.0, END)
+            editor.insert(1.0, contenido)
+
+            detected_language = detect_language(contenido)
+            mensaje_text.delete(1.0, END + "-1c")  # Limpiamos el widget de mensajes
+            if detected_language:
+                mensaje = f"Este código parece estar escrito en {detected_language}."
+                print("Lenguaje detectado:", detected_language)
+            else:
+                mensaje = "No se pudo detectar el lenguaje con certeza."
+            # mensaje_text.insert(END, "prueba uno")   
+            mensaje_text.insert(END, mensaje) 
+            print("Mensaje insertado")
+    except Exception as e:
+        print("Error:", str(e))
+        messagebox.showerror("Error al abrir el archivo", str(e))
+
 def guardar():
     opciones = [("Documento de pearl", "*.pl"), ("Documento de julia", "*.jl"), ("Documento de ruby", "*.rb")]
     documento = asksaveasfile(filetypes=opciones, defaultextension=".pl")
@@ -146,15 +171,12 @@ if __name__ == "__main__":
 
 
     editor = Text(ventana, undo="true")
+    editor.pack(side=LEFT, fill=BOTH, expand=True)
 
-    editor.pack(side=TOP, fill=BOTH, expand=1)
-
-
+    mensaje_text = Text(ventana, undo=False)
+    mensaje_text.pack(side=LEFT, fill=BOTH, expand=True)
 
     ventana.title("Codigazo: Bloc de notas")
-
     ventana.geometry("800x500")
-
     ventana.config(menu=menubar)
-
     ventana.mainloop()
