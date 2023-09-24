8370a735758f1503d65fb41ebc46f296ee2c4ecc
Marc064 <95501065+Marc064@users.noreply.github.com>
2023-09-23 22:08:07 -0500

    Full recognition of programming languages and their paradigms

--- Note.py
+++ Note.py
@@ -4,9 +4,6 @@ import re
 from tkinter import *
 from tkinter import messagebox
 from tkinter.filedialog import askopenfile, asksaveasfile
-from tkinter.scrolledtext import ScrolledText  # Use ScrolledText for text with scrollbar
-from pygments.lexers import get_lexer_for_filename
-from pygments.util import ClassNotFound
 
 
 def update_line_numbers(event=None):
@@ -16,46 +13,64 @@ def update_line_numbers(event=None):
     for line in range(1, line_count + 1):
         line_numbers.insert(END, f"{line}\n")
     line_numbers.config(state=DISABLED)
+    contenido = editor.get(1.0, "end-1c")
+    language = detect_language(contenido)
+    mensaje_text.delete(1.0, END + "-1c")  # Limpiamos el widget de mensajes
+    messaje_paradigm = ""
+    if language:
+        messaje_languaje = f"Este código parece estar escrito en {language}.\n\n"
+        print("Lenguaje detectado:", language)
+        paradigm = detect_paradigm(language, contenido)
+        if paradigm:
+            messaje_paradigm = f"Paradigma: Programación {paradigm} "
+        else:
+            messaje_paradigm = "No se pudo detectar el paradigma"
+    else:
+        messaje_languaje = "No se pudo detectar el lenguaje con certeza."
+    mensaje_text.insert(END, messaje_languaje + messaje_paradigm)
+
 
 def scroll_text_sync(*args):
     editor.yview(*args)
     line_numbers.yview(*args)
 
-def copiar():
 
+def copy():
     editor.clipboard_clear()
 
     editor.clipboard_append(editor.selection_get())
 
-def pegar():
 
+def paste():
     editor.insert(INSERT, editor.clipboard_get())
 
-def cortar():
 
+def cut():
     editor.clipboard_clear()
 
     editor.clipboard_append(editor.selection_get())
 
     editor.delete("sel.first", "sel.last")
 
-def deshacer():
 
+def undo():
     editor.edit_undo()
 
-def rehacer():
 
+def redo():
     editor.edit_redo()
 
-def nuevo():
 
-    editor.delete(1.0,END)
+def new():
+    editor.delete(1.0, END)
+
 
 def detect_language(content):
     patterns = {
-        'julia': r'\b(julia|end|function|if|else)\b',
-        'perl': r'\b(perl|print|sub|if|else)\b',
-        'ruby': r'\b(ruby|puts|def|if|else)\b'
+        'julia': r'\b(julia|function|using|global|let|struct|importall|println)\b',
+        'ruby': r'\b(ruby|require|puts|def|alias|class|elsif|module)\b',
+        'perl': r'\b(perl|use|print|sub|__DATA__|__END__|rand|split|local|exec)\b'
+
     }
 
     detected_language = None
@@ -67,7 +82,61 @@ def detect_language(content):
 
     return detected_language
 
-def abrir():
+
+def detect_paradigm(language, content):
+    if language == "ruby":
+        patterns = {
+            'estructurada': r'\b(if|else|while|for|unless|until|case|when|elsif|else|do)\b',
+            'funcional': r'\b(map|select|reduce|lambda|proc)\b',
+            'orientada a objetos': r'\b(class|module|def|@\w+|\w+\s*=\s*\w+\.new)\b'
+        }
+
+        detected_paradigm = None
+
+        for paradigm, pattern in patterns.items():
+            if re.search(pattern, content):
+                detected_paradigm = paradigm
+                break
+
+        return detected_paradigm
+
+    elif language == "perl":
+        patterns = {
+            'estructurada': r'\b(if|else|while|for|foreach|until|given|when)\b',
+            'funcional': r'\b(map|grep|shift)\b',
+            'orientada a objetos': r'\b(package|new|bless|\$self)\b'
+        }
+
+        detected_paradigm = None
+
+        for paradigm, pattern in patterns.items():
+            if re.search(pattern, content, re.IGNORECASE):
+                detected_paradigm = paradigm
+                break
+
+        return detected_paradigm
+
+
+    elif language == "julia":
+
+        patterns = {
+            'estructurada': r'\b(if|else|while|for|break|continue)\b',
+            'funcional': r'\b(map|filter|reduce|lambda)\b',
+            'orientada a objetos': r'\b(type|mutable|immutable|struct|function|end)\b'
+
+        }
+
+        detected_paradigm = None
+
+        for paradigm, pattern in patterns.items():
+            if re.search(pattern, content):
+                detected_paradigm = paradigm
+                break
+
+        return detected_paradigm
+
+
+def open_file():
     try:
         documento = askopenfile()
         if documento:
@@ -77,21 +146,14 @@ def abrir():
             editor.delete(1.0, END)
             editor.insert(1.0, contenido)
 
-            detected_language = detect_language(contenido)
-            mensaje_text.delete(1.0, END + "-1c")  # Limpiamos el widget de mensajes
-            if detected_language:
-                mensaje = f"Este código parece estar escrito en {detected_language}."
-                print("Lenguaje detectado:", detected_language)
-            else:
-                mensaje = "No se pudo detectar el lenguaje con certeza."
-            # mensaje_text.insert(END, "prueba uno")
-            mensaje_text.insert(END, mensaje)
+            update_line_numbers()
             print("Mensaje insertado")
     except Exception as e:
         print("Error:", str(e))
         messagebox.showerror("Error al abrir el archivo", str(e))
 
-def guardar():
+
+def save():
     opciones = [("Documento de pearl", "*.pl"), ("Documento de julia", "*.jl"), ("Documento de ruby", "*.rb")]
     documento = asksaveasfile(filetypes=opciones, defaultextension=".pl")
 
@@ -107,90 +169,61 @@ def guardar():
         else:
             print("No se especificó una extensión al guardar.")
 
-def acerca():
 
-    messagebox.showinfo("Acerca de Bloc de notas Codigazo", "Bloc de notas Codigazo es un lector de archivos de texto plano,"
+def about():
+    messagebox.showinfo("Editor auto detector de lenguaje",
+                        "El editor logra detectar si se esta usando uno de tres tipos"
 
-                                                         " realizar una aplicación de este tipo ayuda al estudiante a practicar"
+                        " de lenguajes. Perl, Ruby o Julia.\n\n"
 
-                                                         " el uso de interfaces gráficas, la manipulación de archivos de texto"
+                        "Para esto se hace uso de las expresiones regulares"
 
-                                                         " y algunas funciones extra."
-
-                                                         "\n\n"
-
-                                                         "A pesar de ser sencillo tenemos 2 funcionalidades que no tiene el"
-
-                                                         " bloc de notas de Windows, la primera es la opción de rehacer,"
-
-                                                         " la segunda característica es que se puede deshacer o rehacer tantas veces"
-
-                                                         " como se desee a diferencia del bloc denotas de Windows que solo permite"
-
-                                                         " deshacer 2 veces y la segunda cuenta como un rehacer.")
-
-
-
+                        " y algunas funciones extra.\n\n")
 
 
 if __name__ == "__main__":
-
-
-
     ventana = Tk()
-
-
 
     menubar = Menu(ventana)
 
     archivo = Menu(menubar, tearoff=0)
 
-    archivo.add_command(label="Nuevo     ", command=nuevo)
+    archivo.add_command(label="Nuevo     ", command=new)
 
-    archivo.add_command(label="Abrir     ", command=abrir)
+    archivo.add_command(label="Abrir     ", command=open_file)
 
-    archivo.add_command(label="Guardar     ", command=guardar)
+    archivo.add_command(label="Guardar     ", command=save)
 
     archivo.add_command(label="Salir     ", command=ventana.quit)
 
     menubar.add_cascade(label="Archivo", menu=archivo)
 
-
-
     editar = Menu(menubar, tearoff=0)
 
-    editar.add_command(label="Deshacer     ", command=deshacer)
+    editar.add_command(label="Deshacer     ", command=undo)
 
-    editar.add_command(label="Rehacer     ", command=rehacer)
+    editar.add_command(label="Rehacer     ", command=redo)
 
     editar.add_separator()
 
-    editar.add_command(label="Copiar     ", command=copiar)
+    editar.add_command(label="Copiar     ", command=copy)
 
-    editar.add_command(label="Pegar     ", command=pegar)
+    editar.add_command(label="Pegar     ", command=paste)
 
-    editar.add_command(label="Cortar     ", command=cortar)
+    editar.add_command(label="Cortar     ", command=cut)
 
     menubar.add_cascade(label="Edición", menu=editar)
 
-
-
     ayuda = Menu(menubar, tearoff=0)
 
-    ayuda.add_command(label="Acerca de Bloc de notas ", command=acerca)
+    ayuda.add_command(label="Acerca de Bloc de notas ", command=about)
 
     menubar.add_cascade(label="Ayuda", menu=ayuda)
-
-    
 
     line_numbers = Text(ventana, width=4, undo=False, state=DISABLED)
     line_numbers.pack(side=LEFT, fill=Y)
 
-
-    
-
-
-    editor = Text(ventana, undo="true")
+    editor = Text(ventana, undo=True)
     editor.pack(side=LEFT, fill=BOTH, expand=True)
 
     mensaje_text = Text(ventana, undo=False)
@@ -204,6 +237,6 @@ if __name__ == "__main__":
     editor.config(yscrollcommand=scroll_text_sync)
 
     ventana.title("Interprete")
-    ventana.geometry("800x500")
+    ventana.geometry("880x500")
     ventana.config(menu=menubar)
     ventana.mainloop()
